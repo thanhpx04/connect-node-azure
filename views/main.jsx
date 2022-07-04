2fbc715f04d0a0ac1763c7201ea062db4eef2413
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-07-04 18:20:49 +0700

    UI kit package

--- views/main.jsx
+++ views/main.jsx
@@ -1,25 +1,39 @@
 import SectionMessage from '@atlaskit/section-message';
 import React from 'react';
+import ForgeUI, { useState, Button, Macro, render } from '@forge/ui';
 
-export default function HelloWorld() {
-  const [excitementLevel, setExcitementLevel] = React.useState(0);
-  return <SectionMessage
-      title={`Hello, world${excitementLevel ? new Array(excitementLevel).fill('!').join('') : '.'}`}
-      actions={[
-        {
-          key: '1',
-          href: 'https://atlassian.design/components/',
-          text: 'Browse more components to add to your app',
-        },
-        {
-          key: '2',
-          onClick: () => setExcitementLevel(excitementLevel + 1),
-          text: 'Get excited!',
-        }
-      ]}
-    >
-      <p>
-        Congratulations! You have successfully created an Atlassian Connect app using the <a href={'https://bitbucket.org/atlassian/atlassian-connect-express'}>atlassian-connect-express</a> client library.
-      </p>
-    </SectionMessage>;
-}
+const App = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <Button
+      text={`Count is ${count}`}
+      onClick={() => {
+        setCount(count + 1);
+      }}
+    />
+  );
+};
+
+export const run = render(<Macro app={<App />} />);
+// export default function HelloWorld() {
+//   const [excitementLevel, setExcitementLevel] = React.useState(0);
+//   return <SectionMessage
+//       title={`Hello, world${excitementLevel ? new Array(excitementLevel).fill('!').join('') : '.'}`}
+//       actions={[
+//         {
+//           key: '1',
+//           href: 'https://atlassian.design/components/',
+//           text: 'Browse more components to add to your app',
+//         },
+//         {
+//           key: '2',
+//           onClick: () => setExcitementLevel(excitementLevel + 1),
+//           text: 'Get excited!',
+//         }
+//       ]}
+//     >
+//       <p>
+//         Congratulations! You have successfully created an Atlassian Connect app using the <a href={'https://bitbucket.org/atlassian/atlassian-connect-express'}>atlassian-connect-express</a> client library.
+//       </p>
+//     </SectionMessage>;
+// }
