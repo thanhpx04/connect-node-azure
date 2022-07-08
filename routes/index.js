0065ed0ca447a14d6dbc6dc37ead0389ba080513
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-07-08 11:10:30 +0700

    new update

--- routes/index.js
+++ routes/index.js
@@ -3,46 +3,65 @@ export default function routes(app, addon) {
     res.redirect("/atlassian-connect.json");
   });
 
-  app.get("/main", (req, res) => {
-    const { issueKey } = req.query;
-    getIssuesOfCurrentUser();
-    getIssueSummary(addon, req, issueKey).then((data) => {
-      res.render("hello-world.hbs", {
-        issueKey: issueKey,
-        data: data
-      });
-    });
-  });
-
-  async function getIssueSummary(addon, req, issueKey) {
-    return new Promise((resolve, reject) => {
-      var httpClient = addon.httpClient(req);
-      httpClient.get(`/rest/api/2/issue/${issueKey}/changelog`, function (err, res, body) {
-        var listHistoryStoryPoint = JSON.parse(body).values.filter(history => history.items.some(item => item.field === 'Story Points'));
-        var newestStoryPoint = getNewestHistoryStatus(listHistoryStoryPoint);
-        var curentStoryPoint = newestStoryPoint.items.find((item) => {
-          return item.field === "Story Points";
-        });
-        resolve(curentStoryPoint.toString);
-      });
-    });
-  }
-
-  const getNewestHistoryStatus = (listHistoryStoryPoint) => {
-    // array length 0 or 1
-    var newestStoryPoint = listHistoryStoryPoint[0];
-    // array length bigger than 1
-    if (listHistoryStoryPoint.length > 1) {
-      // new array avoid Mutation in JavaScript
-      const listStatusOrderedCreatedDates = [...listHistoryStoryPoint].sort(
-        function (current, next) {
-          return Date.parse(next.created) - Date.parse(current.created);
+    app.get("/main", addon.authenticate(), (req, res) => {
+      const { issueKey } = req.query;
+      getIssueSummary(addon, req, issueKey).then((summary) => {
+        if (summary) {
+          res.render("main.hbs", {
+            summary: summary
+          });
+        } else {
+          res.render("nodata.hbs");
         }
-      );
-      newestStoryPoint = listStatusOrderedCreatedDates[0];
-    }
-    return newestStoryPoint;
-  };
+      });
+    });
 
-  // Add additional route handlers here...
+    async function getIssueSummary(addon, req, issueKey) {
+      return new Promise((resolve, reject) => {
+        var httpClient = addon.httpClient(req);
+        httpClient.get(`/rest/api/2/issue/${issueKey}/changelog`, function (err, res, body) {
+          var listHistoryStatus = JSON.parse(body).values.filter(history => history.items.some(item => item.fieldId === 'status'));
+          var newestStatus = getNewestHistoryStatus(listHistoryStatus);
+          var result = calculateMillisecond(newestStatus);
+          resolve(result);
+        });
+      });
+    }
+
+    const getNewestHistoryStatus = (listHistoryStatus) => {
+      // array length 0 or 1
+      var newestStatus = listHistoryStatus[0];
+      // array length bigger than 1
+      if (listHistoryStatus.length > 1) {
+        // new array avoid Mutation in JavaScript
+        const listStatusOrderedCreatedDates = [...listHistoryStatus].sort(
+          function (current, next) {
+            return Date.parse(next.created) - Date.parse(current.created);
+          }
+        );
+        newestStatus = listStatusOrderedCreatedDates[0];
+      }
+
+      return newestStatus;
+    }
+    
+    const calculateMillisecond = (historyStatus) => {
+      if (historyStatus) {
+        var difMillisecond = new Date() - new Date(historyStatus.created); // milliseconds between now & newestStatus
+        var difDays = Math.floor(difMillisecond / 86400000); // days
+        var difHours = Math.floor((difMillisecond % 86400000) / 3600000); // hours
+        var difMinutes = Math.round(((difMillisecond % 86400000) % 3600000) / 60000); // minutes
+        var curentStatus = historyStatus.items.find((item) => {
+          return item.fieldId === "status";
+        });
+        return {
+          status: curentStatus.toString,
+          days: difDays,
+          hours: difHours,
+          minutes: difMinutes
+        };
+      } else return null;
+    }
+
+    // Add additional route handlers here...
 }
