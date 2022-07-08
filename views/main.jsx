50ff9b5b549039454d565c5330d583c8136f1253
APAC\PAT3HC <thanh.PhamXuan@vn.bosch.com>
2022-07-08 14:30:09 +0700

    fix

--- views/main.jsx
+++ views/main.jsx
@@ -1,5 +1,5 @@
-// import React from "react";
-import ForgeUI, { Button, Fragment, Text, Badge } from "@forge/ui";
+import React from "react";
+// import ForgeUI, { Button, Fragment, Text, Badge } from "@forge/ui";
 
 export default function HelloWorld() {
   return (
